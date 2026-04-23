1870141b02eb947c0a4f4ef49424671c1734054c
Pavibritta <pavithrarajase1@gmail.com>
2026-04-23 15:50:42 +0530

    modified

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(
   }),
 );
 app.use(cookieParser());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ||10000;
 app.use("/", authRoutes);
 app.use("/categories", categoryRoutes);
 app.use("/products", productRoutes);
